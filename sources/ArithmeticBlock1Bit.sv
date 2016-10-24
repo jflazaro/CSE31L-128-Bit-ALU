181333348e850663037502cc91275b400c54dc82
Joshua Lazaro <Joshua Lazaro>
2016-10-23 18:04:28 -0700

    Added ArithmeticBlock1Bit

---
+++ sources/ArithmeticBlock1Bit.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/23/2016 06:00:00 PM
+// Design Name: 
+// Module Name: ArithmeticBlock1Bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ArithmeticBlock1Bit(
+
+    );
+endmodule
---
+++ testbenches/ArithmeticBlock1Bit_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/23/2016 06:01:03 PM
+// Design Name: 
+// Module Name: ArithmeticBlock1Bit_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ArithmeticBlock1Bit_tb(
+
+    );
+endmodule
